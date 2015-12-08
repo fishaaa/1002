9376f5b75c1857e8e9cceafbb904358f71ef28ce
fish <87162122@qq.com>
2015-12-08 17:52:21 +0800

    Verson2
    找到并修正了错误，测试通过
    Memory: 14924K  Time: 1141MS
    需修改算法

--- 1002/1002/main.cpp
+++ 1002/1002/main.cpp
@@ -1,84 +1,100 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <time.h>
+#include <algorithm>
 using namespace std;
-void Quicksort(vector<string> &s,int p,int n)//快速排序法
-{
-	if(p>n)
-		return;
-	string key=s[n];
-	int i=p-1;
-	for(int j=p;j<=n-1;++j){
-		if(s[j]<=key){
-			++i;
-			string temp=s[j];
-			s[j]=s[i];
-			s[i]=temp;
-		}
-	}
-	string temp2=s[n];s[n]=s[i+1];s[i+1]=temp2;
-	int q=i+1;
-	Quicksort(s,p,q-1);
-	Quicksort(s,q+1,n);
-}
 
+//int rand_fish(int m,int n)
+//{
+//	srand((unsigned)time(NULL));
+//	return m+(rand()%(n-m+1));
+//}
+
+//void Quicksort(vector<string> &s,int p,int n)//随机快速排序法
+//{
+//	if(p>=n)
+//		return;
+//	int u=rand_fish(p,n);
+//	string t1=s[u];s[u]=s[n];s[n]=t1;
+//	string key=s[n];
+//	int i=p-1;
+//	for(int j=p;j<=n-1;++j){
+//		if(s[j]<=key){
+//			++i;
+//			string temp=s[j];
+//			s[j]=s[i];
+//			s[i]=temp;
+//		}
+//	}
+//	string temp2=s[n];s[n]=s[i+1];s[i+1]=temp2;
+//	int q=i+1;
+//	Quicksort(s,p,q-1);
+//	Quicksort(s,q+1,n);
+//}
+
+//void Quicksort(vector<string> &s,int p,int n)//快速排序法
+//{
+//	if(p>=n)
+//		return;
+//	string key=s[n];
+//	int i=p-1;
+//	for(int j=p;j<=n-1;++j){
+//		if(s[j]<=key){
+//			++i;
+//			string temp=s[j];
+//			s[j]=s[i];
+//			s[i]=temp;
+//		}
+//	}
+//	string temp2=s[n];s[n]=s[i+1];s[i+1]=temp2;
+//	int q=i+1;
+//	Quicksort(s,p,q-1);
+//	Quicksort(s,q+1,n);
+//}
 vector<string> remain_num(string s[],int n)
 {
 	vector <string> ss(n);
-	for(int i=0;i<n;++i){
-		for(string::iterator it=s[i].begin();it!=s[i].end();++it){
-			if((*it)!='-')
-				ss[i].push_back(*it);
-		}
-	}
-	for(int i=0;i<n;++i){
-		for(int j=0;j<ss[i].size();++j){
-			if(ss[i][j]=='A'||ss[i][j]=='B'||ss[i][j]=='C')
-				ss[i][j]='2';
-			else if(ss[i][j]=='D'||ss[i][j]=='E'||ss[i][j]=='F')
-				ss[i][j]='3';
-			else if(ss[i][j]=='G'||ss[i][j]=='H'||ss[i][j]=='I')
-				ss[i][j]='4';
-			else if(ss[i][j]=='J'||ss[i][j]=='K'||ss[i][j]=='L')
-				ss[i][j]='5';
-			else if(ss[i][j]=='M'||ss[i][j]=='N'||ss[i][j]=='O')
-				ss[i][j]='6';
-			else if(ss[i][j]=='P'||ss[i][j]=='R'||ss[i][j]=='S')
-				ss[i][j]='7';
-			else if(ss[i][j]=='T'||ss[i][j]=='U'||ss[i][j]=='V')
-				ss[i][j]='8';
-			else if(ss[i][j]=='W'||ss[i][j]=='X'||ss[i][j]=='Y')
-				ss[i][j]='9';
+		for(int i=0;i<n;++i){
+ 			for(string::iterator it=s[i].begin();it!=s[i].end();++it){
+				if((*it)=='-'||(*it)=='Q'||(*it)=='Z')
+						continue;
+				else{
+					if((*it)=='A'||(*it)=='B'||(*it)=='C')
+						(*it)='2';
+					else if((*it)=='D'||(*it)=='E'||(*it)=='F')
+						(*it)='3';
+					else if((*it)=='G'||(*it)=='H'||(*it)=='I')
+						(*it)='4';
+					else if((*it)=='J'||(*it)=='K'||(*it)=='L')
+						(*it)='5';
+					else if((*it)=='M'||(*it)=='N'||(*it)=='O')
+						(*it)='6';
+					else if((*it)=='P'||(*it)=='R'||(*it)=='S')
+						(*it)='7';
+					else if((*it)=='T'||(*it)=='U'||(*it)=='V')
+						(*it)='8';
+					else if((*it)=='W'||(*it)=='X'||(*it)=='Y')
+						(*it)='9';
+					ss[i].push_back(*it);
+				}		
 			}
-	
-	}
+		}
 	return ss; 
 }
-vector<string> remain_size(vector<string>ss,int n)
-{
-	int m=0;//record the num that satisfied size 7;
-	for(int i=0;i<n;++i){
-		if(ss[i].size()==7)
-			++m;
-	}
-	vector<string>s_m(m);//用来记录剔除不满size7的电话号码
-	int l=0; //记录符合条件的次数
-	for(int i=0;i<n;++i){
-		if(ss[i].size()!=7)
-			continue;
-		else{
-			s_m[l]=ss[i];
-			++l;
-		}
-	}
-	return s_m;
-}
+
 vector<string> compare(vector<string>sm,int n,vector <int>&num)
-{
-	Quicksort(sm,0,n-1);
+{	
 	vector <string> s_new;
+	if(n==1){
+		s_new=sm;
+		num.push_back(1);
+		return s_new;
+	}
+	//Quicksort(sm,0,n-1); //随机快速排序 复杂度O(nlgn)
+	sort(sm.begin(),sm.end());  
 	int k=1;//记录出现次数
-	for(int i=0;i<n;++i){
+	for(int i=0;i<n;++i){   
 			if(i==n-1)
 			{
 				if(sm[i]!=sm[i-1]){
@@ -93,9 +109,8 @@ vector<string> compare(vector<string>sm,int n,vector <int>&num)
 					break;
 				}
 			}
-			if(sm[i]==sm[i+1]){
+			if(sm[i]==sm[i+1])
 				++k;
-			}
 			else{
 				num.push_back(k);
 				k=1;
@@ -113,26 +128,23 @@ int main()
 		for(int i=0;i<n;++i){
 			cin>>s[i];
 		}
-		vector <string> ss=remain_num(s,n);//除去输入里面的字母与‘-’
-		vector <string> sm=remain_size(ss,n);//除去不满size7后 剩下的string 数组
-		if(sm.size()==0)
-			cout<<"No duplicates."<<endl;
-		else{
-			vector <int>  num;
-			vector<string> result=compare(sm,n,num);
-			for(int i=0;i<num.size();++i){
-				if(num[i]>=2){
-					for(int j=0;j<3;++j)
-						cout<<result[i][j];
-					cout<<"-";
-					for(int j=3;j<result[i].size();++j)
-						cout<<result[i][j];
-					cout<<" "<<num[i]<<endl;	
-				}
-				else
-					continue;
+		vector <string> sm=remain_num(s,n);//除去输入里面的字母与‘-’ 复杂度 O(n)
+		vector <int>  num;
+		vector<string> result=compare(sm,n,num);//O(nlgn+n)
+		int q=0;
+		for(int i=0;i<num.size();++i){//O(n)
+			if(num[i]>=2){
+				result[i].insert(3,"-");
+				cout<<result[i]<<" "<<num[i]<<endl;
+			}
+			else{
+				++q;
+				continue;
 			}
 		}
+		if(q==num.size())
+			cout<<"No duplicates."<<endl;
+		delete []s;
 	}
 	system("pause");
 	return 0;
